102093f274a1e0ae4adc3a3069cbf12a1b89c726
Paolo Henry Reaño Delgadillo <henry29.2004@hotmail.com>
2024-06-23 16:58:43 -0500

    feat: Pantallas de Modelo de negocio y Beneficios añadida

--- js/script.js
+++ js/script.js
@@ -45,11 +45,26 @@ function loadDefaultForm() {
     loadDefaultForm();
       
     // Comportamiento de botones de barra nav
+    var boton_logo = document.getElementById('logo');
     var boton_nav1 = document.getElementById('load-signup');
     var boton_nav2 = document.getElementById('load-about');
     var boton_nav3 = document.getElementById('load-users');
-    
+    var boton_nav4 = document.getElementById('load-benefits');
+    var boton_nav5 = document.getElementById('load-model');
+
     // Event listener para botones
+    boton_logo.addEventListener('click', function(event) {
+      event.preventDefault();
+      fetch('snippets/snippet_sign_up.html')
+                .then(response => response.text())
+                .then(data => {
+                  document.getElementById('main-content').innerHTML = data;
+                  loadDefaultForm();
+                })
+                .catch(error => {
+                  console.error('Error al cargar el contenido dinamico del formulario:', error);
+                }); 
+    });
     boton_nav1.addEventListener('click', function(event) {
       event.preventDefault();
       fetch('snippets/snippet_sign_up.html')
@@ -76,6 +91,28 @@ function loadDefaultForm() {
           boton_nav3.addEventListener('click', function(event) {
       event.preventDefault();
       fetch('snippets/snippet_users.html')
+                .then(response => response.text())
+                .then(data => {
+                  document.getElementById('main-content').innerHTML = data;
+                })
+                .catch(error => {
+                  console.error('Error al cargar el contenido dinamico del formulario:', error);
+                });
+    });
+    boton_nav4.addEventListener('click', function(event) {
+      event.preventDefault();
+      fetch('snippets/snippet_benefits.html')
+                .then(response => response.text())
+                .then(data => {
+                  document.getElementById('main-content').innerHTML = data;
+                })
+                .catch(error => {
+                  console.error('Error al cargar el contenido dinamico del formulario:', error);
+                });
+    });
+    boton_nav5.addEventListener('click', function(event) {
+      event.preventDefault();
+      fetch('snippets/snippet_business_model.html')
                 .then(response => response.text())
                 .then(data => {
                   document.getElementById('main-content').innerHTML = data;
